cd56b22e1abde90340780c24915ab146178d57a8
iput2023-oikawa <tk210503ryo.oikawa@gmail.com>
2023-12-21 14:33:28 +0900

    ブラウザ表示

--- static/js/script.js
+++ static/js/script.js
@@ -18,16 +18,19 @@ async function callApi(method, data = null, id = null){
 document.getElementById("get").addEventListener("click", async () => {
     const result = await callApi("GET");
     console.log(result);
+    displayData(result);
 });
 
 document.getElementById("post").addEventListener("click", async () => {
-    const postData = { title: "test_task"};
+    const taskTitle = document.getElementById("taskTitle").value;
+    const postData = { title: taskTitle};
     const result = await callApi("POST", postData);
     console.log(result);
 });
 
 document.getElementById("put").addEventListener("click", async () => {
-    const putData = { title: "さらなる更新を目指して"};
+    const taskTitle = document.getElementById("taskTitle").value;
+    const putData = { title: taskTitle};
     const taskId = prompt("updateしたいidを入力")
     const result = await callApi("PUT", putData, taskId);
     console.log(result);
@@ -38,3 +41,14 @@ document.getElementById("delete").addEventListener("click", async () => {
     const result = await callApi("DELETE", null, taskId);
     console.log(result);
 });
+
+function displayData(data) {
+    const dataDisplay = document.getElementById("dataDisplay");
+    // const formattedJson = JSON.stringify(data, null, 2);
+    // const htmlFormattedJson = formattedJson.replace(/\n/g, `<br>`);
+    // dataDisplay.innerHTML = htmlFormattedJson;
+    data.forEach(item => {
+        const itemString = JSON.stringify(item, null, 2);
+        dataDisplay.innerHTML += `{<br>${itemString}<br>}<br>`;
+    })
+}
